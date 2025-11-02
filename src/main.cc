ca746de2a00fe64bcf99420b2432672f36571010
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2025-11-02 13:10:34 -0300

    add minimal raylib code, glfw3

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,20 @@
 #include <iostream>
 
+#include <raylib.h>
+
 int main(void) {
     std::cout << "Hello, World!" << std::endl;
+
+    InitWindow(800, 600, "Hello World");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Hello, World!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
